000fc9b90604c37017ed972cafdcc8ce7999af57
Stella Achar Oiro <stella.achar@azubiafrica.org>
2023-06-11 10:13:19 -0700

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
-FROM python:3.10-slim
+FROM python:3.9-slim
+
 WORKDIR /app
+
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-COPY main.py .
-EXPOSE 8000
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 8091
+
+CMD ["uvicorn", "API_app:app", "--host", "0.0.0.0", "--port", "8091", "--reload"]
--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# Define your ML model input schema
+class PredictionInput(BaseModel):
+    PRG: int
+    PL: int
+    PR: int
+    SK: int
+    TS: int
+    M11: float
+    BD2: float
+    Age: int
+    Insurance: int
+
+# Load the exported objects
+with open('/content/encoder.pkl', 'rb') as file:
+    encoder = pickle.load(file)
+
+with open('/content/scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+with open('/content/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Create the FastAPI app
+app = FastAPI(title='Sepsis API', description='An API that takes input and displays the predictions', version='0.1.0')
 
 # Define the prediction endpoint
-@app.post("/predict")
-def predict_sepsis(data: InputData):
-    # Convert input data to a feature vector
-    feature_vector = [[
-        data.Age,
-        data.Insurance,
-        data.PRG,
-        data.PL,
-        data.PR,
-        data.SK,
-        data.TS,
-        data.M11
-    ]]
+@app.post('/Sepsis')
+def predict(input_data: PredictionInput):
+    # Preprocess the input features
+    encoded_features = encoder.transform([[input_data.PRG, input_data.PL, input_data.PR, input_data.SK, input_data.TS, input_data.M11, input_data.BD2, input_data.Age, input_data.Insurance]])
+    scaled_features = scaler.transform(encoded_features)
 
-    # Make the prediction
-    prediction = model.predict(feature_vector)
-    probability = model.predict_proba(feature_vector)[:, 1]
+    # Make predictions using the model
+    predictions = model.predict(scaled_features)
 
-    # Prepare the response
-    response = {
-        "prediction": prediction[0],
-        "probability": probability[0]
-    }
+    # Labeling Model output
+    if prediction[0] < 0.5:
+        prediction_label = "Negative. This person does not have Sepsis."
+    else:
+        prediction_label = "Positive. This person has Sepsis."
 
-    return response
+    # Return the predictions
+    return {'predictions': predictions.tolist()}
+
+
